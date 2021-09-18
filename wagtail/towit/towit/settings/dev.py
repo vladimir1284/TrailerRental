e94e1154998379b55a52798bd9baccfbca7090be
vladimir1284 <vladimir.rdguez@gmail.com>
2021-09-17 21:56:02 -0400

    Initial wagtail file

---
+++ wagtail/towit/towit/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-a7!cigmf2d9)mjz@gx7ny#uahd%b*#@k1hq3(ftu#lwb0q@dd4'
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ['*'] 
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
