59bd9b826a6106cc53c8afbd69edcdf3891047a0
vladimir1284 <vladimir.rdguez@gmail.com>
2022-01-30 00:58:09 -0500

    Remove url import

--- trailer_rental/towit/urls.py
+++ trailer_rental/towit/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import include, url
+from django.conf.urls import include
 from django.urls import path
 from django.contrib.auth import views as auth_views
 from towit.views import *
